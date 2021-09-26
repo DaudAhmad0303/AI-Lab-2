c25869d3bf60245efa8fb18a0218068561ca288e
Daud Ahmad <daudahmad0303@gmail.com>
2021-09-26 21:41:49 +0500

    comment updated

--- Task 1.py
+++ Task 1.py
@@ -1,7 +1,7 @@
 def print_range(s = 1500, e = 2701):
     '''
-    Finds those numbers which are divisible by 7 and multiple of 
-    5, between 1500 and 2700 (both included)
+    Finds those numbers which are divisible by 7 and multiple of
+    5, between 1500 and 2700 (both included).
     '''
     for i in range(1500, 2701):
         if i % 7 == 0 and i % 5 == 0:
